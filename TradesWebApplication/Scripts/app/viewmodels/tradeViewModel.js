244749bc34d1994826016b17edaf24a74b8717a5
apatel7886 <info@threeiconsulting.com>
2013-12-18 19:24:37 -0500

    fixed json prblems for ko vm, ready to do test post to server

--- TradesWebApplication/Scripts/app/viewmodels/tradeViewModel.js
+++ TradesWebApplication/Scripts/app/viewmodels/tradeViewModel.js
@@ -1,39 +1,39 @@
 ﻿
 
-function TradeLine(id, positionId, financialInstrumentId) {
+function TradeLine(trade_line_id, position_id, tradable_thing_id) {
     var self = this;
-    id = typeof (id) !== 'undefined' ? id : 0;
-    this.id = ko.observable(id);
+    trade_line_id = typeof (trade_line_id) !== 'undefined' ? trade_line_id : 0;
+    this.trade_line_id = ko.observable(trade_line_id);
 
-    positionId = typeof (positionId) !== 'undefined' ? positionId : 0;
-    this.positionId = ko.observable(positionId);
+    position_id = typeof (position_id) !== 'undefined' ? position_id : 0;
+    this.positionId = ko.observable(position_id);
 
-    financialInstrumentId = typeof (financialInstrumentId) !== 'undefined' ? financialInstrumentId : 0;
-    this.financialInstrumentId = ko.observable(financialInstrumentId);
+    tradable_thing_id = typeof (tradable_thing_id) !== 'undefined' ? tradable_thing_id : 0;
+    this.tradable_thing_id = ko.observable(tradable_thing_id);
 
 }
 
-function TradeGroup(id, groupStructureId, groupCanonicalLabel, groupEditorialLabel, tradeLines) {
+function TradeGroup(trade_line_group_id, trade_line_group_type_id, trade_line_group_label, trade_line_group_editorial_label, tradeLines) {
     var self = this;
 
-    id = typeof (id) !== 'undefined' ? id : 0;
-    this.id = ko.observable(id);
+    trade_line_group_id = typeof (trade_line_group_id) !== 'undefined' ? trade_line_group_id : 0;
+    this.trade_line_group_id = ko.observable(trade_line_group_id);
 
-    groupStructureId = typeof (groupStructureId) !== 'undefined' ? groupStructureId : 0;
-    this.groupStructureId = ko.observable(groupStructureId);
+    trade_line_group_type_id = typeof (trade_line_group_type_id) !== 'undefined' ? trade_line_group_type_id : 0;
+    this.trade_line_group_type_id = ko.observable(trade_line_group_type_id);
 
-    groupCanonicalLabel = typeof (groupCanonicalLabel) !== 'undefined' ? groupCanonicalLabel : "";
-    this.groupCanonicalLabel = ko.observable(groupCanonicalLabel);
+    trade_line_group_label = typeof (trade_line_group_label) !== 'undefined' ? trade_line_group_label : "";
+    this.trade_line_group_label = ko.observable(trade_line_group_label);
 
-    groupEditorialLabel = typeof (groupEditorialLabel) !== 'undefined' ? groupEditorialLabel : "";
-    this.groupEditorialLabel = ko.observable(groupEditorialLabel);
+    trade_line_group_editorial_label = typeof (trade_line_group_editorial_label) !== 'undefined' ? trade_line_group_editorial_label : "";
+    this.trade_line_group_editorial_label = ko.observable(trade_line_group_editorial_label);
 
     tradeLines = typeof (tradeLines) !== 'undefined' ? tradeLines : [];
     this.tradeLines = ko.observableArray(tradeLines);
 
-    this.addLine = function (id) {
-        self.tradeLines.push(new TradeLine(id));
-        ko.applyBindings(vm);
+    this.addLine = function () {
+        var nextIndex = this.tradeLines().length;
+        self.tradeLines.push(new TradeLine(nextIndex));
     };
 
     this.removeLine = function (item) {
@@ -42,68 +42,74 @@ function TradeGroup(id, groupStructureId, groupCanonicalLabel, groupEditorialLab
 }
 
 function TradeViewModel(
-  id,
-  serviceId,
-  tradeTypeId,
-  benchmarkId,
-  benchMarkSelectionId,
-  systemUpdatedCreatedDate,
-  tradeCanonicalLabel,
-  tradeEditorialLabel,
-  tradeStructureId,
-  instructionEntry,
-  instructionEntryDate,
-  instructionExit,
-  instructionExitDate,
-  instructionTypeId,
-  hedgeId,
-  relatedTrades,
-  aplFunc,
+  trade_id,
+  service_id,
+  length_type_id,
+  relativity_id,
+  benchmark_id,
+  created_on,
+  trade_label,
+  trade_editorial_label,
+  structure_type_id,
+  instruction_entry,
+  instruction_entry_date,
+  instruction_exit,
+  instruction_exit_date,
+  instruction_type_id,
+  hedge_id,
+  currency_id,
+  related_trade_ids,
+  apl_func,
   mark_to_mark_rate,
   interest_rate_diff,
   abs_measure_type_id,
   abs_currency_id,
+  abs_return_value,
   rel_measure_type_id,
   rel_currency_id,
+  rel_return_value,
   return_benchmark_id,
   comments
   ) {
     var self = this;
 
-    id = typeof (id) !== 'undefined' ? id : 0;
-    this.id = ko.observable(id);
+    trade_id = typeof (id) !== 'undefined' ? trade_id : 0;
+    this.trade_id = ko.observable(trade_id);
 
     //form values - top
-    serviceId = typeof (serviceId) !== 'undefined' ? serviceId : 0;
-    this.serviceId = ko.observable(serviceId);
+    service_id = typeof (serviceId) !== 'undefined' ? service_id : 0;
+    this.service_id = ko.observable(service_id);
 
-    tradeTypeId = typeof (tradeTypeId) !== 'undefined' ? tradeTypeId : 0;
-    this.tradeTypeId = ko.observable(tradeTypeId);
+    length_type_id = typeof (length_type_id) !== 'undefined' ? length_type_id : 0;
+    this.length_type_id = ko.observable(length_type_id);
+    relativity_id
 
-    benchmarkId = typeof (benchmarkId) !== 'undefined' ? benchmarkId : 0;
-    this.benchmarkId = ko.observable(benchmarkId);
+    relativity_id = typeof (relativity_id) !== 'undefined' ? relativity_id : 0;
+    this.relativity_id = ko.observable(relativity_id);
 
-    benchMarkSelectionId = typeof (benchMarkSelectionId) !== 'undefined' ? benchMarkSelectionId : 0;
-    this.benchMarkSelectionId = ko.observable(benchMarkSelectionId);
+    benchmark_id = typeof (benchmark_id) !== 'undefined' ? benchmark_id : 0;
+    this.benchmark_id = ko.observable(benchmark_id);
 
-    systemUpdatedCreatedDate = typeof (systemUpdatedCreatedDate) !== 'undefined' ? systemUpdatedCreatedDate : "";
-    this.systemUpdatedCreatedDate = ko.observable(systemUpdatedCreatedDate);
+    created_on = typeof (created_on) !== 'undefined' ? created_on : "";
+    this.created_on = ko.observable(created_on);
 
 
-    tradeCanonicalLabel = typeof (tradeCanonicalLabel) !== 'undefined' ? tradeCanonicalLabel : "";
-    this.tradeCanonicalLabel = ko.observable(tradeCanonicalLabel);
+    trade_label = typeof (trade_label) !== 'undefined' ? trade_label : "";
+    this.trade_label = ko.observable(trade_label);
 
-    tradeEditorialLabel = typeof (tradeEditorialLabel) !== 'undefined' ? tradeEditorialLabel : "";
-    this.tradeEditorialLabel = ko.observable(tradeEditorialLabel);
+    trade_editorial_label = typeof (trade_editorial_label) !== 'undefined' ? trade_editorial_label : "";
+    this.trade_editorial_label = ko.observable(trade_editorial_label);
 
-    tradeStructureId = typeof (tradeStructureId) !== 'undefined' ? tradeStructureId : 0;
-    this.tradeStructureId = ko.observable(tradeStructureId);
+    structure_type_id = typeof (structure_type_id) !== 'undefined' ? structure_type_id : 0;
+    this.structure_type_id = ko.observable(structure_type_id);
 
     //tradegroups
     this.tradegroups = ko.observableArray([]);
 
     this.addGroup = function () {
-        self.tradegroups.push(new TradeGroup());
+        var nextIndex = this.tradegroups().length;
+        var newGroup = new TradeGroup(nextIndex,1,"","",[new TradeLine()]);
+        self.tradegroups.push(newGroup);
     };
 
     this.removeGroup = function (item) {
@@ -112,32 +118,32 @@ function TradeViewModel(
     //end tradegroups
 
     //form values - bottom
-    instructionEntry = typeof (instructionentry) !== 'undefined' ? instructionEntry : "";
-    this.instructionEntry = ko.observable(instructionEntry);
+    instruction_entry = typeof (instruction_entry) !== 'undefined' ? instruction_entry : "";
+    this.instruction_entry = ko.observable(instruction_entry);
 
-    instructionEntryDate = typeof (instructionEntryDate) !== 'undefined' ? instructionEntryDate : "";
-    this.instructionEntryDate = ko.observable(instructionEntryDate);
+    instruction_entry_date = typeof (instruction_entry_date) !== 'undefined' ? instruction_entry_date : "";
+    this.instruction_entry_date = ko.observable(instruction_entry_date);
 
-    instructionExit = typeof (instructionExit) !== 'undefined' ? instructionExit : "";
-    this.instructionExit = ko.observable(instructionExit);
+    instruction_exit = typeof (instruction_exit) !== 'undefined' ? instruction_exit : "";
+    this.instruction_exit = ko.observable(instruction_exit);
 
-    instructionExitDate = typeof (instructionExitDate) !== 'undefined' ? instructionExitDate : "";
-    this.instructionExitDate = ko.observable(instructionExitDate);
+    instruction_exit_date = typeof (instruction_exit_date) !== 'undefined' ? instruction_exit_date : "";
+    this.instruction_exit_date = ko.observable(instruction_exit_date);
 
-    instructionTypeId = typeof (instructionTypeId) !== 'undefined' ? instructionTypeId : 0;
-    this.instructionTypeId = ko.observable(instructionTypeId);
+    instruction_type_id = typeof (instruction_type_id) !== 'undefined' ? instruction_type_id : 0;
+    this.instruction_type_id = ko.observable(instruction_type_id);
 
-    hedgeId = typeof (hedgeId) !== 'undefined' ? hedgeId : 0;
-    this.hedgeId = ko.observable(hedgeId);
+    hedge_id = typeof (hedge_id) !== 'undefined' ? hedge_id : 0;
+    this.hedge_id = ko.observable(hedge_id);
 
-    currencyId = typeof (currencyId) !== 'undefined' ? currencyId : 0;
-    this.currencyId = ko.observable(currencyId);
+    currency_id = typeof (currency_id) !== 'undefined' ? currency_id : 0;
+    this.currency_id = ko.observable(currency_id);
 
-    relatedTrades = typeof (relatedTrades) !== 'undefined' ? relatedTrades : [];
-    this.relatedTrades = ko.observableArray(relatedTrades);
+    related_trade_ids = typeof (related_trade_ids) !== 'undefined' ? related_trade_ids : [];
+    this.related_trade_ids = ko.observableArray(related_trade_ids);
 
-    aplFunc = typeof (aplFunc) !== 'undefined' ? aplFunc : "";
-    this.aplFunc = ko.observable(aplFunc);
+    apl_func = typeof (apl_func) !== 'undefined' ? apl_func : "";
+    this.apl_func = ko.observable(apl_func);
 
     mark_to_mark_rate = typeof (mark_to_mark_rate) !== 'undefined' ? mark_to_mark_rate : "";
     this.mark_to_mark_rate = ko.observable(mark_to_mark_rate);
@@ -151,11 +157,17 @@ function TradeViewModel(
     abs_currency_id = typeof (abs_currency_id) !== 'undefined' ? abs_currency_id : 0;
     this.abs_currency_id = ko.observable(abs_currency_id);
 
+    abs_return_value = typeof (abs_return_value) !== 'undefined' ? abs_return_value : "";
+    this.abs_return_value = ko.observable(abs_return_value);
+
     rel_measure_type_id = typeof (rel_measure_type_id) !== 'undefined' ? rel_measure_type_id : 0;
     this.rel_measure_type_id = ko.observable(rel_measure_type_id);
 
     rel_currency_id = typeof (rel_currency_id) !== 'undefined' ? rel_currency_id : 0;
     this.rel_currency_id = ko.observable(rel_currency_id);
+
+    rel_return_value = typeof (rel_return_value) !== 'undefined' ? rel_return_value : "";
+    this.rel_return_value = ko.observable(rel_return_value);
 
     return_benchmark_id = typeof (return_benchmark_id) !== 'undefined' ? return_benchmark_id : 0;
     this.return_benchmark_id = ko.observable(return_benchmark_id);
@@ -163,20 +175,6 @@ function TradeViewModel(
     comments = typeof (comments) !== 'undefined' ? comments : "";
     this.comments = ko.observable(comments);
 
-}
-
-function SubmitCreatedTrade()
-{
-    var jsonData = ko.toJSON(vm);
-    $.post("/Trade/Create", jsonData, function (returnedData) {
-        // This callback is executed if the post was successful    
-    })
-}
-
-
-function RefreshVM() {
-    alert('Trying to refresh!');
-    ko.applyBindings(vm);
 }
 
 var vm = new TradeViewModel();
