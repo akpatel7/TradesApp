c2f5eb02d986c571ad83a3ea11226df84ede6bfb
Amit Patel <patel_amitk@hotmail.com>
2013-12-21 18:45:59 -0500

    main fields validating, next: data type validations, canonical label

--- TradesWebApplication/ViewModels/TradesCreationViewModel.cs
+++ TradesWebApplication/ViewModels/TradesCreationViewModel.cs
@@ -17,86 +17,93 @@ namespace TradesWebApplication.ViewModels
         public List<TradeLineViewModel> TradeLines { get; set; }
 
         // service
-        [Required(ErrorMessage = "required")]
-        [DisplayName("Service")]
+        [Required(ErrorMessage = "Required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please make a selection")]
+        [DisplayName("Service;")]
         public int service_id { get; set; }
         public List<Service> Services { get; set; }
         
         // trade type
-        [Required(ErrorMessage = "required")]
-        [DisplayName("Trade Type")]
+        [Required(ErrorMessage = "Required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please make a selection")]
+        [DisplayName("Trade Type:")]
         public int length_type_id { get; set; }
         public List<Length_Type> LengthTypes { get; set; }
         
         // benchmark
-        [Required(ErrorMessage = "required")]
-        [DisplayName("Benchmark")]
+        [Required(ErrorMessage = "Required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please make a selection")]
+        [DisplayName("Benchmark:")]
         public int relativity_id { get; set; }
         public List<Relativity> Relativitys { get; set; }
         
         // benchmark selection
-        [DisplayName("Benchmark Selection")]
+         [DisplayName("Benchmark Selection:")]
+        //[Range(1, int.MaxValue, ErrorMessage = "Please make a selection")]
         public int benchmark_id { get; set; }
         public List<Benchmark> Benchmarks { get; set; }
         
         // update time
-        [Required(ErrorMessage = "required")]
-        [DisplayName("System Updated/Created Date")]
+        [Required(ErrorMessage = "Required")]
+        [DisplayName("System Updated/Created Date;")]
         public string created_on { get; set; }
         
         // canonical label
-        [DisplayName("Canonical Label")]
+        [DisplayName("Canonical Label:")]
         public string trade_label { get; set; }
         
         // editorial label
-        [DisplayName("Editorial Label")]
+        [DisplayName("Editorial Label:")]
         public string trade_editorial_label { get; set; }
         
         // trade structure
-        [Required(ErrorMessage = "required")]
-        [DisplayName("Trade Structure")]
+        [Required(ErrorMessage = "Required")]
+        [DisplayName("Trade Structure:")]
         public int structure_type_id { get; set; }
         public List<Structure_Type> StructureTypes { get; set; }
 
         
         //------------------------------------
         // entry level
-        [Required(ErrorMessage = "required")]
-        [DisplayName("Entry Level")]
+        [Required(ErrorMessage = "Required")]
+        [Range(0d, double.MaxValue, ErrorMessage = "Please enter a value")]
+        [DisplayName("Entry Level:")]
         public decimal? instruction_entry { get; set; }
         
         // start date
-        [Required(ErrorMessage = "required")]
-        [DisplayName("Start Date")]
+        [Required(ErrorMessage = "Required")]
+        [DisplayName("Start Date:")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public string instruction_entry_date { get; set; }
         
         // exit level
-        [DisplayName("Exit Level")]
+        [DisplayName("Exit Level:")]
         public Nullable<decimal> instruction_exit { get; set; }
 
         // exit date
-        [DisplayName("Exit Date")]
+        [DisplayName("Exit Date:")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public string instruction_exit_date { get; set; }
 
         // instruction
-        [DisplayName("Instruction")]
+        [DisplayName("Instruction:")]
         public Nullable<int> instruction_type_id { get; set; }
         public List<Instruction_Type> InstructionTypes { get; set; }
         public string instruction_label { get; set; }
 
         // hedge instruction
-        [DisplayName("Hedge Instructions")]
+        [DisplayName("Hedge Instructions:")]
         public Nullable<int> hedge_id { get; set; }
         public List<Hedge_Type> HedgeTypes { get; set; }
 
         // curency
-        [DisplayName("Trade Execution Currency")]
+        [DisplayName("Trade Execution Currency:")]
         public Nullable<int> currency_id { get; set; }
         public List<Currency> Currencies { get; set; }
 
         // supplementary info
         // linked trades
-        [DisplayName("Linked Trades")]
+        [DisplayName("Linked Trades:")]
         public int[] related_trade_ids { get; set; }
         public List<Trade> RelatedTrades { get; set; }
 
@@ -104,34 +111,34 @@ namespace TradesWebApplication.ViewModels
 
         public Trade_Performance TradePerformance { get; set; }
         // APL function
-        [DisplayName("APL Function")]
+        [DisplayName("APL Function:")]
         public string apl_func { get; set; }
 
         // mark to mark rate
-        [DisplayName("Mark to Market Rate")]
+        [DisplayName("Mark to Market Rate:")]
         public string mark_to_mark_rate { get; set; }
         
         // internset rate diff
-        [DisplayName("Interest Rate Differential")]
+        [DisplayName("Interest Rate Differential;")]
         public string interest_rate_diff { get; set; }
         
         // abs performance
         public virtual ICollection<Measure_Type> MeasureTypes { get; set; }
-        [DisplayName("Return")]
+        [DisplayName("Return:")]
         public Nullable<int> abs_measure_type_id { get; set; }
         public Nullable<int> abs_currency_id { get; set; }
         // rel performance
-        [DisplayName("Return")]
+        [DisplayName("Return:")]
         public Nullable<int> rel_measure_type_id { get; set; }
         public Nullable<int> rel_currency_id { get; set; }
-        [DisplayName("Relative to")]
+        [DisplayName("Relative to:")]
         public Nullable<int> return_benchmark_id { get; set; }
         // return value
         public string abs_return_value { get; set; }
         public string rel_return_value { get; set; }
 
         // comments
-        [DisplayName("Comments")]
+        [DisplayName("Comments:")]
         public string Comments { get; set; }
         public Trade_Comment Comment { get; set; }
 
