ea3a1afe389272924361df14d7f18a4699056723
apatel7886 <info@threeiconsulting.com>
2014-03-06 17:35:27 -0500

    TA-57 View Trades List Complete not 45...

--- TradesWebApplication/Scripts/app/viewmodels/tradeEditViewModel.js
+++ TradesWebApplication/Scripts/app/viewmodels/tradeEditViewModel.js
@@ -1505,7 +1505,7 @@ function TradeViewModel(
     
     //set this to true to see ko.toJson on form for debugging knockout bindings
     this.APItester = new ResultViewModel();
-    this.debug = true;
+    this.debug = false;
     //////////////////
 }
 
